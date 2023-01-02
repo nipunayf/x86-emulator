4729bd23163abe4712b59717dd45826a41a51629
nipunayf <nipuna.18@cse.mrt.ac.lk>
2023-01-02 20:00:08 +0530

    Add include guards to the headers

--- src/modrm.hpp
+++ src/modrm.hpp
@@ -1,3 +1,6 @@
+#ifndef MODRM_H
+#define MODRM_H
+
 #include "memory.hpp"
 #include "register_bank.hpp"
 
@@ -9,3 +12,5 @@
 
 void processModRM(const uint8_t &byte, RegisterBank &regBank, Memory &memory,
                   uint32_t &operandRM, uint32_t &operandReg);
+
+#endif
--- src/sib.hpp
+++ src/sib.hpp
@@ -1,3 +1,8 @@
+#ifndef SIB_H
+#define SIB_H
+
 #include "register_bank.hpp"
 
 uint32_t processSIB(const uint8_t &sib, RegisterBank &regBank);
+
+#endif
