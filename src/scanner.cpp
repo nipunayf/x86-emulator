44a9bed25daa041285a19fb0e5cb7fb3dd3d84e2
nipunayf <nipuna.18@cse.mrt.ac.lk>
2023-01-03 02:14:47 +0530

    Generate 8bit output from Scanner

--- src/scanner.cpp
+++ src/scanner.cpp
@@ -5,17 +5,17 @@ Scanner::Scanner(const std::string &file_path) {
   this->file.open(file_path);
   if (!this->file) {
     std::cerr << "Invalid file path: " << file_path << std::endl;
-    exit(0);
+    exit(1);
   }
   this->file >> std::hex;
 }
 
 Scanner::~Scanner() { this->file.close(); }
 
-uint16_t Scanner::next() {
-  uint16_t nextByte;
-  if (file >> nextByte) {
-    return nextByte;
+uint8_t Scanner::next() {
+  uint16_t byte;
+  if (file >> byte) {
+    return (uint8_t)byte;
   }
   return 0;
 }
