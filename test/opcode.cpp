d29c98ced9f490cfcc05038eab5f899eea47e8e4
nipunayf <nipuna.18@cse.mrt.ac.lk>
2023-01-04 23:51:05 +0530

    Modularize asserting program deaths

--- test/opcode.cpp
+++ test/opcode.cpp
@@ -2,32 +2,27 @@
 #include "test_utils.hpp"
 #include <gtest/gtest.h>
 
-#define OPCODE_TEST_PATH (TEST_PATH + "/opcode/")
+#define OPCODE_TEST_PATH (TEST_PATH + "opcode/")
 
-TEST(OpcodeTest, InvalidOneByte) {
-  Scanner scanner(OPCODE_TEST_PATH + "invalid_1byte.txt");
-  ASSERT_DEATH({ parse(scanner); },
-               "Invalid opcode: 0x99 is not yet supported");
-}
+TEST(OpcodeTest, InvalidOneByte){
+  ASSERT_X86_DEATH("invalid_1byte.txt",
+                   "Invalid opcode: 0x99 is not yet supported")}
 
-TEST(OpcodeTest, InvalidTwoBytes) {
-  Scanner scanner(OPCODE_TEST_PATH + "invalid_2bytes.txt");
-  ASSERT_DEATH({ parse(scanner); },
-               "Invalid opcode bytes: 2 is not yet supported");
-}
+TEST(OpcodeTest, InvalidTwoBytes){
+  ASSERT_X86_DEATH("invalid_2bytes.txt",
+                   "Invalid opcode bytes: 2 is not yet supported")}
 
-TEST(OpcodeTest, InvalidThreeBytes) {
-  Scanner scanner(OPCODE_TEST_PATH + "invalid_3bytes.txt");
-  ASSERT_DEATH({ parse(scanner); },
-               "Invalid opcode bytes: 3 is not yet supported");
-}
+TEST(OpcodeTest, InvalidThreeBytes){
+  ASSERT_X86_DEATH("invalid_3bytes.txt",
+                   "Invalid opcode bytes: 3 is not yet supported")}
 
-TEST(OpcodeTest, InvalidSecondByteOfFourBytes) {
-  Scanner scanner(OPCODE_TEST_PATH + "invalid_2byte_4bytes.txt");
-  ASSERT_DEATH({ parse(scanner); }, "Expected 0xf after 0x66");
-}
+TEST(OpcodeTest, InvalidSecondByteOfFourBytes){
+  ASSERT_X86_DEATH("invalid_2byte_4bytes.txt", "Expected 0xf after 0x66")}
 
 TEST(OpcodeTest, ValidOneByte) {
   Scanner scanner(OPCODE_TEST_PATH + "valid_1byte.txt");
-  ASSERT_FALSE(parse(scanner));
+  RegisterBank reg_bank;
+  Memory memory;
+  Argument args{scanner, reg_bank, memory};
+  ASSERT_FALSE(parse(args));
 }
--- test/test_utils.hpp
+++ test/test_utils.hpp
@@ -5,4 +5,13 @@
 
 #define TEST_PATH std::filesystem::current_path().string() + "/res/"
 
+#define ASSERT_X86_DEATH(path, err_msg)                                        \
+  {                                                                            \
+    Scanner scanner(OPCODE_TEST_PATH + path);                                  \
+    RegisterBank reg_bank;                                                     \
+    Memory memory;                                                             \
+    Argument args{scanner, reg_bank, memory};                                  \
+    ASSERT_DEATH({ parse(args); }, err_msg);                                   \
+  }
+
 #endif
