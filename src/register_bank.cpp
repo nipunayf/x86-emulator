c31bcc8e0f1b4e7714b59288ab22ef024d6c3ed0
poorna2152 <poorna2152@gmail.com>
2023-01-04 11:57:20 +0530

    Add loading of register value for increment

--- src/register_bank.cpp
+++ src/register_bank.cpp
@@ -1,16 +1,48 @@
 #include "register_bank.hpp"
 
+#define MASK_32 4294967295 << 32
+#define MASK_16 MASK_32 | (65535 << 16)
+#define MASK_L8 MASK_16 | (255 << 8)
+#define MASK_H8 MASK_16 | 255
+
 RegisterBank::RegisterBank() {
-  registers[EAX] = Register({"eax", 0xbf8db144});
-  registers[ECX] = Register({"ecx", 0x88c5cffb});
-  registers[EDX] = Register({"edx", 0x1});
-  registers[EBX] = Register({"ebx", 0xae5ff4});
-  registers[ESP] = Register({"esp", 0xbf8db0bc});
-  registers[EBP] = Register({"ebp", 0xbf8db118});
-  registers[ESI] = Register({"esi", 0x9a0ca0});
-  registers[EDI] = Register({"edi", 0x0});
+  registers[RAX] = Register({"rax", 0xbf8db144});
+  registers[RCX] = Register({"rcx", 0x88c5cffb});
+  registers[RDX] = Register({"rdx", 0x1});
+  registers[RBX] = Register({"rbx", 0xae5ff4});
+  registers[RSP] = Register({"rsp", 0xbf8db0bc});
+  registers[RBP] = Register({"rbp", 0xbf8db118});
+  registers[RSI] = Register({"rsi", 0x9a0ca0});
+  registers[RDI] = Register({"rdi", 0x0});
 }
 
 uint64_t RegisterBank::load64(const uint32_t &index) {
   return registers[index].value;
+}
+
+uint32_t RegisterBank::load32(const uint32_t &index) {
+  return registers[index].value;
+}
+
+uint16_t RegisterBank::load16(const uint32_t &index) {
+  return registers[index].value;
+}
+
+void RegisterBank::set8(const uint32_t &index, const uint8_t value) {
+  if (index % 2 == 1)
+    registers[index / 2].value = (registers[index].value & MASK_L8) | value;
+  else
+    registers[index / 2].value = (registers[index].value & MASK_H8) | value;
+}
+
+void RegisterBank::set16(const uint32_t &index, const uint16_t value) {
+  registers[index].value = (registers[index].value & MASK_16) | value;
+}
+
+void RegisterBank::set32(const uint32_t &index, const uint32_t value) {
+  registers[index].value = (registers[index].value & MASK_32) | value;
+}
+
+void RegisterBank::set64(const uint32_t &index, const uint64_t value) {
+  registers[index].value = value;
 }
--- src/sib.cpp
+++ src/sib.cpp
@@ -9,12 +9,12 @@ uint32_t get_scaled_index(const uint8_t &scale, const uint8_t &index,
               << std::endl;
     exit(1);
   }
-  return reg_bank.load(index) * pow(2, scale);
+  return reg_bank.load32(index) * pow(2, scale);
 }
 
 uint32_t process_sib(const uint8_t &sib, RegisterBank &reg_bank) {
   const uint8_t base = sib & 0x07;
   const uint8_t index = (sib >> 3) & 0x07;
   const uint8_t scale = (sib >> 6) & 0x03;
-  return reg_bank.load(base) + get_scaled_index(scale, index, reg_bank);
+  return reg_bank.load32(base) + get_scaled_index(scale, index, reg_bank);
 }
