b4464a11623260cafd16f7a3db67a4083ffe4796
nipunayf <nipuna.18@cse.mrt.ac.lk>
2023-01-05 00:13:33 +0530

    Modularize formatting decoded instruction

--- src/utils.cpp
+++ src/utils.cpp
@@ -6,3 +6,20 @@ std::string format_hex_string(uint32_t byte) {
   string_stream << std::hex << byte;
   return "0x" + string_stream.str();
 }
+
+
+std::string format_instruction(const std::string &operand,
+                               const std::string &first_param) {
+  char instruction[8];
+  sprintf(instruction, "%s\t%%%s", operand.c_str(), first_param.c_str());
+  return instruction;
+}
+
+std::string format_instruction(const std::string &operand,
+                               const std::string &first_param,
+                               const std::string &second_param) {
+  char instruction[14];
+  sprintf(instruction, "%s\t%%%s, %%%s", operand.c_str(), first_param.c_str(),
+          second_param.c_str());
+  return instruction;
+}
