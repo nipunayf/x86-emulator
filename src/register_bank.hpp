af6707fd7687948e18002e08106171133c914dcc
poorna2152 <poorna2152@gmail.com>
2023-01-04 10:46:46 +0530

    Add enums

--- src/register_bank.hpp
+++ src/register_bank.hpp
@@ -3,28 +3,50 @@
 
 #include <string>
 
-#define EAX 0
-#define ECX 1
-#define EDX 2
-#define EBX 3
-#define ESP 4
-#define EBP 5
-#define ESI 6
-#define EDI 7
+enum x86_8bit_registers { AL, CL, DL, BL, AH, CH, DH, BH };
+
+enum x86_16bit_registers { AX, CX, DX, BX, SP, BP, SI, DI };
+
+enum x86_32bit_registers { EAX, ECX, EDX, EBX, ESP, EBP, ESI, EDI };
+
+enum x86_64bit_registers {
+  RAX,
+  RCX,
+  RDX,
+  RBX,
+  RSP,
+  RBP,
+  RSI,
+  RDI,
+  R8,
+  R9,
+  R10,
+  R11,
+  R12,
+  R13,
+  R14,
+  R15
+};
 
 struct Register {
   std::string name;
-  uint32_t value;
+  uint64_t value;
 };
 
 class RegisterBank {
 private:
   Register registers[8];
-  std::string name;
 
 public:
   RegisterBank();
-  uint32_t load(const uint32_t &index);
+  uint8_t load8(const uint32_t &index);
+  uint16_t load16(const uint32_t &index);
+  uint32_t load32(const uint32_t &index);
+  uint64_t load64(const uint32_t &index);
+  void set8(const uint32_t &index, const uint8_t value);
+  void set16(const uint32_t &index, const uint16_t value);
+  void set32(const uint32_t &index, const uint32_t value);
+  void set64(const uint32_t &index, const uint64_t value);
 };
 
 #endif
