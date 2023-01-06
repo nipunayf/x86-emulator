a7f5cfb763651ad1307a7c0c8c69b384460e4f69
nipunayf <nipuna.18@cse.mrt.ac.lk>
2023-01-06 23:17:27 +0530

    Support register type in modmr

--- src/modrm.cpp
+++ src/modrm.cpp
@@ -1,76 +1,114 @@
 #include "modrm.hpp"
 #include "sib.hpp"
+#include <iostream>
 #include <string>
 
-uint32_t register_direct(const uint8_t &reg, RegisterBank &reg_bank) {
-  return reg_bank.load32(reg);
+uint32_t register_direct(RegisterBank &reg_bank, const uint8_t &reg,
+                         RegisterType reg_type, std::string &reg_name) {
+  switch (reg_type) {
+    // TODO: Uncomment after implementation of register 8
+    //  case REGISTER_8:
+    //    reg_name = reg_bank.name8(reg);
+    //    return reg_bank.load8(reg);
+  case REGISTER_16:
+    reg_name = reg_bank.name16(reg);
+    return reg_bank.load16(reg);
+  case REGISTER_32:
+    reg_name = reg_bank.name32(reg);
+    return reg_bank.load32(reg);
+  case REGISTER_64:
+    reg_name = reg_bank.name64(reg);
+    return reg_bank.load64(reg);
+  }
 }
 
-uint32_t register_indirect(const uint8_t &reg, Scanner &scanner,
-                           RegisterBank &reg_bank, Memory &memory) {
+uint32_t register_direct(RegisterBank &reg_bank, const uint8_t &reg,
+                         RegisterType reg_type) {
+  switch (reg_type) {
+    // TODO: Uncomment after implementation of register 8
+    //  case REGISTER_8:
+    //    return reg_bank.load8(reg);
+  case REGISTER_16:
+    return reg_bank.load16(reg);
+  case REGISTER_32:
+    return reg_bank.load32(reg);
+  case REGISTER_64:
+    return reg_bank.load64(reg);
+  }
+}
+
+uint32_t register_indirect(State &state, const uint8_t &reg,
+                           RegisterType reg_type, std::string &reg_name) {
   uint32_t memory_addr;
   if (reg == ESP) {
-    uint8_t sib = scanner.next_byte();
-    memory_addr = process_sib(sib, reg_bank);
+    uint8_t sib = state.scanner.next_byte();
+    memory_addr = process_sib(sib, state.reg_bank);
   } else if (reg == EBP) {
-    uint32_t displacement = scanner.next_four_bytes();
+    uint32_t displacement = state.scanner.next_nbytes(4);
     memory_addr = displacement;
   } else {
-    memory_addr = reg_bank.load32(reg);
+    memory_addr = register_direct(state.reg_bank, reg, reg_type);
   }
-  return memory.read(memory_addr);
+  reg_name = format_hex_string(memory_addr);
+  return state.memory.read(memory_addr);
 }
 
-uint32_t indirect_one_byte_displacement(const uint8_t &reg, Scanner &scanner,
-                                        RegisterBank &reg_bank,
-                                        Memory &memory) {
+uint32_t indirect_one_byte_displacement(State &state, const uint8_t &reg,
+                                        RegisterType reg_type,
+                                        std::string &reg_name) {
   uint32_t memory_addr;
   uint8_t displacement;
   if (reg == ESP) {
-    uint8_t sib = scanner.next_byte();
-    displacement = scanner.next_byte();
-    memory_addr = process_sib(sib, reg_bank) + displacement;
+    uint8_t sib = state.scanner.next_byte();
+    displacement = state.scanner.next_byte();
+    memory_addr = process_sib(sib, state.reg_bank) + displacement;
   } else {
-    displacement = scanner.next_byte();
-    memory_addr = reg_bank.load32(reg) + displacement;
+    displacement = state.scanner.next_byte();
+    memory_addr = register_direct(state.reg_bank, reg, reg_type) + displacement;
   }
-  return memory.read(memory_addr);
+  reg_name = format_hex_string(memory_addr);
+  return state.memory.read(memory_addr);
 }
 
-uint32_t indirect_four_byte_displacement(const uint8_t &reg, Scanner &scanner,
-                                         RegisterBank &reg_bank,
-                                         Memory &memory) {
+uint32_t indirect_four_byte_displacement(State &state, const uint8_t &reg,
+                                         RegisterType reg_type,
+                                         std::string &reg_name) {
   uint32_t memory_addr, displacement;
   if (reg == ESP) {
-    uint8_t sib = scanner.next_byte();
-    displacement = scanner.next_four_bytes();
-    memory_addr = process_sib(sib, reg_bank) + displacement;
+    uint8_t sib = state.scanner.next_byte();
+    displacement = state.scanner.next_nbytes(4);
+    memory_addr = process_sib(sib, state.reg_bank) + displacement;
   } else {
-    displacement = scanner.next_four_bytes();
-    memory_addr = reg_bank.load32(reg) + displacement;
+    displacement = state.scanner.next_nbytes(4);
+    memory_addr = register_direct(state.reg_bank, reg, reg_type) + displacement;
   }
-  return memory.read(memory_addr);
+  reg_name = format_hex_string(memory_addr);
+  return state.memory.read(memory_addr);
 }
 
-void process_modmr(const uint8_t &byte, Scanner &scanner,
-                   RegisterBank &reg_bank, Memory &memory, uint32_t &operand_rm,
-                   uint32_t &operand_reg) {
-  const uint8_t mode = byte >> 6;
+void process_modmr(State &state, ModmrAttribute &args) {
+  const uint8_t byte = state.scanner.next_byte();
+  const auto mode = static_cast<const AddressingMode>(byte >> 6);
   const uint8_t rm = byte & 0x07;
   const uint8_t reg = (byte >> 3) & 0x07;
+
   switch (mode) {
   case REGISTER_INDIRECT:
-    operand_rm = register_indirect(rm, scanner, reg_bank, memory);
+    args.rm_ptr = register_indirect(state, rm, args.rm_type, args.rm_name);
     break;
   case ONE_BYTE_DISPLACEMENT:
-    operand_rm = indirect_one_byte_displacement(rm, scanner, reg_bank, memory);
+    args.rm_ptr =
+      indirect_one_byte_displacement(state, rm, args.rm_type, args.rm_name);
     break;
   case FOUR_BYTE_DISPLACEMENT:
-    indirect_four_byte_displacement(rm, scanner, reg_bank, memory);
+    args.rm_ptr =
+      indirect_four_byte_displacement(state, rm, args.rm_type, args.rm_name);
     break;
   case REGISTER_DIRECT:
-    operand_rm = register_direct(rm, reg_bank);
+    args.rm_ptr =
+      register_direct(state.reg_bank, rm, args.rm_type, args.rm_name);
     break;
   }
-  operand_reg = register_direct(reg, reg_bank);
+  args.reg_ptr =
+    register_direct(state.reg_bank, reg, args.reg_type, args.reg_name);
 }
