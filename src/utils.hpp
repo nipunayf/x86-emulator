988287409c0c82c7316342df1c349a75839ebafc
nipunayf <nipuna.18@cse.mrt.ac.lk>
2023-01-07 12:44:48 +0530

    Add formatting displacement

--- src/utils.hpp
+++ src/utils.hpp
@@ -15,4 +15,6 @@ std::string format_instruction(const std::string &operand,
 std::string format_register_change(const std::string &reg_name,
                                    uint32_t before_value, uint32_t after_value);
 
+std::string format_displacement(uint32_t memory_addr);
+
 #endif
