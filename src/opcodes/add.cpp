01b410c6e1cd863d57eb6b1708f6b4d2f717f664
nipunayf <nipuna.18@cse.mrt.ac.lk>
2023-01-10 19:28:56 +0530

    Rename args to instruction

--- src/opcodes/add.cpp
+++ src/opcodes/add.cpp
@@ -8,7 +8,7 @@ int32_t perform_add(State &state, OperandSize op_size, int32_t op1,
   // Set overflow flag (OF)
   // When two positive integers or two negative integer operands produce a
   // result with a complemented msb
-  state.reg_bank.set_flag(state.args.snapshot.flag_transitions, OF,
+  state.reg_bank.set_flag(state.ins.snapshot.flag_transitions, OF,
                           read_msb(op_size, op1) == read_msb(op_size, op2) &&
                             read_msb(op_size, res) != read_msb(op_size, op1));
   return res;
@@ -30,7 +30,7 @@ void add05(State &state) {
   auto displace = (int32_t)state.scanner.next_nbytes(4);
 
   int32_t output = perform_add(state, OPERAND_32, reg, displace);
-  state.reg_bank.set32(state.args.snapshot.reg_transition, EAX, output);
+  state.reg_bank.set32(state.ins.snapshot.reg_transition, EAX, output);
 
   set_snapshot(state, "add", format_immediate(displace),
                state.reg_bank.name32(EAX));
