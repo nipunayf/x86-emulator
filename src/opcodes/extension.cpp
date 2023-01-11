68ff2971082286f85ce8518bdb245a9aa695b108
nipunayf <nipuna.18@cse.mrt.ac.lk>
2023-01-11 17:50:39 +0530

    Make imm function a template

--- src/opcodes/extension.cpp
+++ src/opcodes/extension.cpp
@@ -8,31 +8,53 @@ uint32_t read_immediate(Scanner &scanner, OperandSize type) {
   return scanner.next_nbytes(pow(2, type));
 }
 
-void ext8x(State &state, OperandSize reg_type, OperandSize im_type) {
-  ModRMAttribute rm_args{reg_type}, reg_args{im_type};
+template <typename T>
+void ext8x(State &state, OperandSize reg_type, OperandSize imm_type) {
+  ModRMAttribute rm_args{reg_type}, reg_args{reg_type};
   process_modrm(state, rm_args, reg_args);
-  uint32_t imm = read_immediate(state.scanner, OPERAND_8);
+  T immediate = read_immediate(state.scanner, imm_type);
   std::string operation;
-  uint32_t output;
+  T res;
   switch (reg_args.reg) {
   case 0:
     operation = "add";
-    output = perform_add(state, reg_type, rm_args.val, imm);
+    res = perform_add<T>(state, reg_type, (T)rm_args.val, immediate);
     break;
   default:
     print_error_and_exit("Instruction %d / %d not yet implemented",
                          format_hex_string(state.ins.opcode), reg_args.reg);
     break;
   }
-  set_value(state, rm_args, output);
-  set_snapshot(state, operation, format_immediate(imm), rm_args.notation);
+  set_value(state, rm_args, res);
+  set_snapshot(state, operation, rm_args.notation, format_immediate(immediate));
 }
 
-void ext80(State &state) { ext8x(state, OPERAND_8, OPERAND_8); }
+void ext80(State &state) { ext8x<int8_t>(state, OPERAND_8, OPERAND_8); }
 
-void ext81(State &state) { ext8x(state, OPERAND_32, OPERAND_32); }
+void ext81(State &state) { ext8x<int32_t>(state, OPERAND_32, OPERAND_32); }
 
-void ext83(State &state) { ext8x(state, OPERAND_32, OPERAND_8); }
+void ext83(State &state) { ext8x<int32_t>(state, OPERAND_32, OPERAND_8); }
+
+void extFE(State &state) {
+  ModRMAttribute rm_args{OPERAND_8}, reg_args{OPERAND_8};
+  process_modrm(state, rm_args, reg_args);
+  std::string operation;
+  int8_t res = 0;
+  switch (reg_args.reg) {
+  case 0:
+    operation = "inc";
+    //TODO: Implement DEC instructions
+    break;
+  case 1:
+    operation = "dec";
+    //TODO: Implement DEC instruction
+    break;
+  default:
+    print_error_and_exit("Invalid reg code %s for '0xfe' opcode", reg_args.reg);
+  }
+  set_value(state, rm_args, res);
+  set_snapshot(state, operation, rm_args.notation);
+}
 
 void extFF(State &state) {
   ModRMAttribute rm_args{OPERAND_32}, reg_args{OPERAND_32};
@@ -67,3 +89,4 @@ void ext8F(State &state) {
   set_value(state, rm_args, output);
   set_snapshot(state, operation, rm_args.notation);
 }
+
