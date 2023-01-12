802fa7d145aeb336b20a1b307067748bee2110e7
poorna2152 <poorna2152@gmail.com>
2023-01-12 16:08:55 +0530

    Add xchg instruction

---
+++ src/opcodes/xchg.cpp
@@ -0,0 +1,21 @@
+#include "xchg.hpp"
+#include "../modrm.hpp"
+
+void xchg9x(State &state) {
+  Register32 reg = (Register32)(state.ins.opcode - 0x90);
+  uint32_t temp = state.reg_bank.load32(EAX);
+  state.reg_bank.set32(state.ins.snapshot.reg_transition, EAX,
+                       state.reg_bank.load32(reg));
+  state.reg_bank.set32(state.ins.snapshot.reg_transition, reg, temp);
+  set_snapshot(state, XCHG_INS, state.reg_bank.name32(EAX),
+               state.reg_bank.name32(reg));
+}
+
+void xchg86_87(State &state) {
+  OperandSize size = state.ins.opcode == 0x86 ? OPERAND_8 : OPERAND_32;
+  ModRMAttribute rm_args{size}, reg_args{size};
+  process_modrm(state, rm_args, reg_args);
+  set_value(state, reg_args, rm_args.val);
+  set_value(state, rm_args, reg_args.val);
+  set_snapshot(state, XCHG_INS, reg_args.notation, rm_args.notation);
+}
---
+++ test/xchg.cpp
@@ -0,0 +1,33 @@
+#include "opcode.hpp"
+#include "test_utils.hpp"
+#include <gtest/gtest.h>
+
+#define XCHG_TEST_PATH (TEST_PATH + "xchg/")
+
+#define XCHG(file_name, dest1, expected1, dest2, expected2)                    \
+  Scanner scanner(XCHG_TEST_PATH + file_name);                                 \
+  RegisterBank reg_bank;                                                       \
+  Memory memory;                                                               \
+  State state{scanner, reg_bank, memory};                                      \
+  parse(state);                                                                \
+  RegisterBank original_reg_bank;                                              \
+                                                                               \
+  ASSERT_EQ(dest1, expected1);                                                 \
+  ASSERT_EQ(dest2, expected2);                                                 \
+  ASSERT_EQ(state.snapshots.size(), 1);
+
+// xchg [edi + 0x11010110], dl
+TEST(XchgTest, Xchg86){XCHG("xchg86.txt", reg_bank.load8(DL), 0,
+                            memory.load8(reg_bank.load32(EDI) + 0x11010110),
+                            original_reg_bank.load8(DL))}
+
+// xchg esi, eax
+TEST(XchgTest, Xchg87){XCHG("xchg87.txt", reg_bank.load32(EAX),
+                            original_reg_bank.load32(ESI), reg_bank.load32(ESI),
+                            original_reg_bank.load32(EAX))}
+
+// xchg eax, edx
+TEST(XchgTest, Xchg92) {
+  XCHG("xchg92.txt", reg_bank.load32(EAX), original_reg_bank.load32(EDX),
+       reg_bank.load32(EDX), original_reg_bank.load32(EAX))
+}
