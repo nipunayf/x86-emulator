f223995b130510554db55bca1822c114bc271607
nipunayf <nipuna.18@cse.mrt.ac.lk>
2023-01-14 01:31:35 +0530

    Add more instructions for jcc

--- src/opcodes/jmp.cpp
+++ src/opcodes/jmp.cpp
@@ -1,7 +1,39 @@
 #include "jmp.hpp"
 
-void jmpEB(State &state) { JUMP(1, JMP_INS, true) }
+void jmpEB(State &state) { JUMP_REL8(JMP_INS, true) }
 
-void jmpE9(State &state) { JUMP(4, JMP_INS, true) }
+void jmpE9(State &state) { JUMP_REL32(JMP_INS, true) }
 
-void jc(State &state) { JUMP(1, "jc", state.reg_bank.load_flag(CF) == 1) }
+void jc72(State &state) { JUMP_REL8("jc", state.reg_bank.load_flag(CF) == 1) }
+
+void jae73(State &state) { JUMP_REL8("jae", state.reg_bank.load_flag(CF) == 0) }
+
+void je74(State &state) { JUMP_REL8("je", state.reg_bank.load_flag(ZF) == 1) }
+
+void jbe76(State &state) {
+  JUMP_REL8("jbe", state.reg_bank.load_flag(CF) == 1 &&
+                     state.reg_bank.load_flag(ZF) == 1)
+}
+
+void ja77(State &state) {
+  JUMP_REL8("ja", state.reg_bank.load_flag(CF) == 0 &&
+                    state.reg_bank.load_flag(ZF) == 0)
+}
+
+void jl7C(State &state) {
+  JUMP_REL8("jl", state.reg_bank.load_flag(SF) != state.reg_bank.load_flag(OF))
+}
+
+void jge7D(State &state) {
+  JUMP_REL8("jge", state.reg_bank.load_flag(SF) == state.reg_bank.load_flag(OF))
+}
+
+void jg7F(State &state) {
+  JUMP_REL8("jg",
+            state.reg_bank.load_flag(ZF) == 0 &&
+              state.reg_bank.load_flag(SF) == state.reg_bank.load_flag(OF))
+}
+
+void jecxzE3(State &state) {
+  JUMP_REL8("jecxz", state.reg_bank.load32(ECX) == 0)
+}
--- src/opcodes/jmp.hpp
+++ src/opcodes/jmp.hpp
@@ -8,12 +8,24 @@
 
 void jmpEB(State &state);
 void jmpE9(State &state);
-void jc(State &state);
+void jc72(State &state);
+void jae73(State &state);
+void je74(State &state);
+void jbe76(State &state);
+void ja77(State &state);
+void jl7C(State &state);
+void jge7D(State &state);
+void jg7F(State &state);
+void jecxzE3(State &state);
 
 #define JUMP(bytes, ins_name, cond)                                            \
   uint8_t displacement = state.ins_fetcher.next_nbytes(bytes);                 \
   if (cond)                                                                    \
     state.ins_fetcher.jump_byte(displacement);                                 \
   set_snapshot(state, ins_name, format_immediate(displacement));
+
+#define JUMP_REL8(ins_name, cond) JUMP(1, ins_name, cond)
+
+#define JUMP_REL32(ins_name, cond) JUMP(4, ins_name, cond)
 
 #endif
