b695238d56a07aea2a5ac260f31aa963098e3153
nipunayf <nipuna.18@cse.mrt.ac.lk>
2023-01-14 00:34:30 +0530

    Implement jmp instruction for E9 code

--- src/instruction_fetcher.hpp
+++ src/instruction_fetcher.hpp
@@ -12,14 +12,14 @@ private:
   Memory &m_memory;
   OperandSize m_mode;
 
-  uint8_t fetch_instruction(uint32_t offset);
+  uint8_t fetch_instruction(int32_t offset);
 
 public:
   InstructionFetcher(RegisterBank &reg_bank, Memory &memory, OperandSize mode);
 
   uint8_t next_byte();
   uint32_t next_nbytes(unsigned short num_bytes);
-  void jump_byte(uint32_t offset);
+  void jump_byte(int32_t offset);
   bool is_eof();
 };
 
--- test/jmp.cpp
+++ test/jmp.cpp
@@ -21,5 +21,27 @@ TEST(JmpTest, JmpEB) {
   uint32_t prev_value = reg_bank.load32(EAX);
   parse(state);
 
+  ASSERT_EQ(state.snapshots.size(), 4);
+  ASSERT_EQ(reg_bank.load32(EAX), prev_value + 3);
+}
+
+// inc eax
+// jmp 0x3000006
+// inc eax
+// inc eax
+// inc eax
+// inc eax
+// inc eax
+TEST(JmpTest, JmpE9) {
+  RegisterBank reg_bank;
+  Memory memory;
+  store_program(JMP_TEST_PATH + "jmpE9.txt", reg_bank, memory);
+  InstructionFetcher ins_fetcher(reg_bank, memory, OPERAND_32);
+  State state{ins_fetcher, reg_bank, memory};
+
+  uint32_t prev_value = reg_bank.load32(EAX);
+  parse(state);
+
+  ASSERT_EQ(state.snapshots.size(), 4);
   ASSERT_EQ(reg_bank.load32(EAX), prev_value + 3);
 }
