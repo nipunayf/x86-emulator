a18e3a59a0f07ab3290042b9fd2ba29bec10f5dc
nipunayf <nipuna.18@cse.mrt.ac.lk>
2023-01-14 01:10:33 +0530

    Implement jcc instruction

--- test/jmp.cpp
+++ test/jmp.cpp
@@ -4,6 +4,19 @@
 
 #define JMP_TEST_PATH (TEST_PATH + "jmp/")
 
+#define JUMP(file_name, snapshot_size)                                         \
+  RegisterBank reg_bank;                                                       \
+  Memory memory;                                                               \
+  store_program(JMP_TEST_PATH + file_name, reg_bank, memory);                  \
+  InstructionFetcher ins_fetcher(reg_bank, memory, OPERAND_32);                \
+  State state{ins_fetcher, reg_bank, memory};                                  \
+                                                                               \
+  uint32_t prev_value = reg_bank.load32(EAX);                                  \
+  parse(state);                                                                \
+                                                                               \
+  ASSERT_EQ(state.snapshots.size(), snapshot_size);                            \
+  ASSERT_EQ(reg_bank.load32(EAX), prev_value + 3);
+
 // inc eax
 // jmp 0x3
 // inc eax
@@ -11,37 +24,26 @@
 // inc eax
 // inc eax
 // inc eax
-TEST(JmpTest, JmpEB) {
-  RegisterBank reg_bank;
-  Memory memory;
-  store_program(JMP_TEST_PATH + "jmpEB.txt", reg_bank, memory);
-  InstructionFetcher ins_fetcher(reg_bank, memory, OPERAND_32);
-  State state{ins_fetcher, reg_bank, memory};
-
-  uint32_t prev_value = reg_bank.load32(EAX);
-  parse(state);
-
-  ASSERT_EQ(state.snapshots.size(), 4);
-  ASSERT_EQ(reg_bank.load32(EAX), prev_value + 3);
-}
+TEST(JmpTest, JmpEB){JUMP("jmpEB.txt", 4)}
 
 // inc eax
-// jmp 0x3000006
+// jmp 0x3
 // inc eax
 // inc eax
 // inc eax
 // inc eax
 // inc eax
-TEST(JmpTest, JmpE9) {
-  RegisterBank reg_bank;
-  Memory memory;
-  store_program(JMP_TEST_PATH + "jmpE9.txt", reg_bank, memory);
-  InstructionFetcher ins_fetcher(reg_bank, memory, OPERAND_32);
-  State state{ins_fetcher, reg_bank, memory};
+TEST(JmpTest, JmpE9){JUMP("jmpE9.txt", 4)}
 
-  uint32_t prev_value = reg_bank.load32(EAX);
-  parse(state);
-
-  ASSERT_EQ(state.snapshots.size(), 4);
-  ASSERT_EQ(reg_bank.load32(EAX), prev_value + 3);
+// inc eax
+// jb 0x3
+// inc eax
+// stc
+// jb 0x3
+// inc eax
+// inc eax
+// inc eax
+// inc eax
+TEST(JmpTest, Jmp72) {
+  JUMP("jmp72.txt", 6)
 }
